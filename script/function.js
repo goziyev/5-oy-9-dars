70310a168cc83e3c8bbb9119506a6b1faf6f4016
goziyev <goziyevmuhammadgozi@gmail.com>
2024-01-16 17:20:39 +0500

    task completed

--- script/function.js
+++ script/function.js
@@ -6,7 +6,7 @@ function createCard(phone) {
     <h3><small class= "fs-5">nomi: </small>${phone.name}</h3>
     
     <p>narxi: ${phone.price}$</p>
-    <p>izoh: ${phone.description.length ? phone.description : "Bu mahsulot bo'yicha qoshimcha malumotlar mavjud emas"}</p>
+    <p class="desc">izoh: ${phone.description.length ? phone.description : "Bu mahsulot bo'yicha qoshimcha malumotlar mavjud emas"}</p>
     <button class="t in-detail" id="element_${phone.id}">Batafsil</button>
 </div>
 
